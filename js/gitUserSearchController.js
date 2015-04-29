9889b7cac02bed95486221e83a60d1a9b9c7c36d
Maciej Kurek <maciejk77@gmail.com>
2015-04-29 11:53:42 +0100

    Mocked API call

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -4,13 +4,14 @@ githubUserSearch.controller('GitUserSearchController', ['$resource', function($r
 
   var accessToken = '3c68bd2439ad3efb99c24453530bddb1b5ea9799'
 
-  var searchResource = $resource('https://api.gitub.com/search/users');
+  var searchResource = $resource('https://api.github.com/search/users');
 
   self.doSearch = function() {
     self.searchResult = searchResource.get(
       {
         q: self.searchTerm,
         access_token: self.accessToken
+      
       }
     );
   };
